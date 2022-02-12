2ba9de7de11a2cedc1fb3998e09235fb0cce5e92
ハムスケ ハム <daiki.esdfth12488@gmail.com>
2022-02-12 11:55:39 +0900

    fix: remove redundant method

--- src/main/java/com/hamusuke/damageindicator/math/AdditionalMathHelper.java
+++ src/main/java/com/hamusuke/damageindicator/math/AdditionalMathHelper.java
@@ -8,15 +8,7 @@ public class AdditionalMathHelper {
         return MathHelper.color((((rgb >> 16) & 0xFF) / 255.0F) * multiplier, (((rgb >> 8) & 0xFF) / 255.0F) * multiplier, ((rgb & 0xFF) / 255.0F) * multiplier);
     }
 
-    public static float lerp(float delta, float start, float end) {
-        return start + delta * (end - start);
-    }
-
     public static float convexUpwardQuadraticFunction(float delta, float minX, float maxX, float minY, float maxY) {
-        return (float) (((minY - maxY) / Math.pow(MathHelper.absMax(minX, maxX), 2)) * Math.pow(lerp(delta, minX, maxX), 2) + maxY);
-    }
-
-    public static double lerp(double delta, double start, double end) {
-        return start + delta * (end - start);
+        return (float) (((minY - maxY) / Math.pow(MathHelper.absMax(minX, maxX), 2)) * Math.pow(MathHelper.lerp(delta, minX, maxX), 2) + maxY);
     }
 }
