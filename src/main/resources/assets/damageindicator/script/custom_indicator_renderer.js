3027fe45d7b1d84791a54f459ba2a4679c5ed0fd
ハムスケ ハム <daiki.esdfth12488@gmail.com>
2022-07-09 19:22:31 +0900

    fix: default script

--- src/main/resources/assets/damageindicator/script/custom_indicator_renderer.js
+++ src/main/resources/assets/damageindicator/script/custom_indicator_renderer.js
@@ -11,8 +11,16 @@ function tick(renderer) {
 }
 
 function calculateScale(timeDelta, renderer) {
-    var scale = Packages.com.hamusuke.damageindicator.math.AdditionalMathHelper.convexUpwardQuadraticFunction(Packages.net.minecraft.util.math.MathHelper.clamp(timeDelta / (12.5 * maxAge), 0.0, 1.0), renderer.crit ? -0.2 : -0.5, renderer.crit ? 2.0 : 0.5, 0.00375 * renderer.distance * 1.732050807 * renderer.scaleMultiplier, 0.0075 * renderer.distance * 1.732050807 * renderer.scaleMultiplier * renderer.scaleMultiplier * (renderer.crit ? 1.0 : 0.8));
+    var scale = Packages.com.hamusuke.damageindicator.math.AdditionalMathHelper.convexUpwardQuadraticFunction(clamp(timeDelta / (12.5 * maxAge), 0.0, 1.0), renderer.crit ? -0.2 : -0.5, renderer.crit ? 2.0 : 0.5, 0.00375 * renderer.distance * 1.732050807 * renderer.scaleMultiplier, 0.0075 * renderer.distance * 1.732050807 * renderer.scaleMultiplier * renderer.scaleMultiplier * (renderer.crit ? 1.0 : 0.8));
     scale -= 0.00025 * renderer.textWidth;
-    scale = Packages.net.minecraft.util.math.MathHelper.clamp(scale, 0.0001, 20.0);
+    scale = clamp(scale, 0.0001, 20.0);
     return scale;
+}
+
+function clamp(number, min, max) {
+    if (number < min) {
+        return min;
+    } else {
+        return number > max ? max : number;
+    }
 }
